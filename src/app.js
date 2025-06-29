fa180a5c18c97c99a92a1315db33965096214fa9
estevam02009 <manoel.neto@53861@alunos.ufersa.edu.br>
2025-06-29 09:53:24 -0300

    Craido o arquivo Server.js e App.ja

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connectDB = require('./config/db');
+const redis = require('./config/redis');
+const authRoutes = require('./routes/authRoutes');
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Rotas
+app.use('/api/auth', authRoutes);
+
+app.use('/', (req, res) => {
+    res.send('API rodando!');
+});
+
+module.exports = app;  // Exporta a aplicação para ser utilizada em outros arquivos
